068dfd52d01b0dee06c7760983177c35ca5872a3
SyedFaisal30 <sfarz172320@gmail.com>
2024-12-21 09:28:51 +0530

    diet page design changes

--- src/app/diet/diet-components/DietNavbar.tsx
+++ src/app/diet/diet-components/DietNavbar.tsx
@@ -1,60 +1,62 @@
 "use client";
 
-import styles from "../diet.module.css";
 import { useState } from "react";
+import styles from "../diet.module.css";
 import Link from "next/link";
 import Image from "next/image";
 
-const DietNavbar: React.FC = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
+const DashboardNavbar: React.FC = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  const toggleMenu = () => {
-    setIsMenuOpen((prev) => !prev);
-  };
+    const toggleMenu = () => {
+        setIsMenuOpen((prev) => !prev);
+    };
 
-  const menuItems = [
-    { name: "Dashboard", link: "/dashboard" },
-    { name: "Exercise", link: "/exercise" },
-    { name: "Diet", link: "/diet" },
-  ];
+    const menuItems = [
+        { name: "Dashboard", link: "/dashboard" },
+        { name: "Exercise", link: "/exercise" },
+        { name: "Diet", link: "/diet" },
+    ];
 
-  return (
-    <nav className={styles.navbar}>
-      <div className={styles.navbarTabs}>
-        <Image
-          src="/Dashboard Images/Icon Black.svg"
-          alt="App Icon"
-          className={styles.logo}
-          width={40}
-          height={40}
-        />
-        <div className={styles.mobileMenu} onClick={toggleMenu}>
-          <Image
-            src={
-              isMenuOpen
-                ? "/Dashboard Images/close.svg"
-                : "/Dashboard Images/hamburger.svg"
-            }
-            alt="Hamburger Icon"
-            width={30}
-            height={30}
-          />
-        </div>
+    return (
+        <nav className={styles.navbar}>
+            <div className={styles.navbarTabs}>
+                <Image
+                    src="/Dashboard Images/Icon Black.svg"
+                    alt="App Icon"
+                    className={styles.logo}
+                    width={40}
+                    height={40}
+                />
+                <div className={styles.mobileMenu} onClick={toggleMenu}>
+                    <Image
+                        src={
+                            isMenuOpen
+                                ? "/Dashboard Images/close.svg"
+                                : "/Dashboard Images/hamburger.svg"
+                        }
+                        alt="Hamburger Icon"
+                        width={30}
+                        height={30}
+                    />
+                </div>
 
-        <div
-          className={`${styles.menuItems} ${isMenuOpen ? styles.menuOpen : ""}`}
-        >
-          <div className={styles.outerMenu}>
-            {menuItems.map((item) => (
-              <Link key={item.link} href={item.link} className={styles.tab}>
-                {item.name}
-              </Link>
-            ))}
-          </div>
-        </div>
-      </div>
-    </nav>
-  );
+                <div
+                    className={`${styles.menuItems} ${
+                        isMenuOpen ? styles.menuOpen : ""
+                    }`}
+                >
+                    <div className={styles.outerMenu}>
+                        {menuItems.map((item) => (
+                            <Link key={item.link} href={item.link} className={styles.tab}>
+                                {item.name}
+                            </Link>
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </nav>
+    );
 };
 
-export default DietNavbar;
+export default DashboardNavbar;
