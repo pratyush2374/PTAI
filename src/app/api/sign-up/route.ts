ac3db56b9dc75475399aa6aecf25836679f6011a
pratyush2374 <pratyushsharma2374@gmail.com>
2024-12-24 18:17:21 +0530

    Sign up integration part 3

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -1,3 +1,4 @@
+import prisma from "@/lib/prismaClient";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
@@ -25,11 +26,80 @@ export async function POST(req: NextRequest) {
             additionalInfo,
         } = await req.json();
 
-        return NextResponse.json({ message: "Received data" }, { status: 200 });
-    } catch (error) {
-        console.log(error);
+        // Deriving username from email and calculating age
+        const username = email.split("@")[0];
+        const age = new Date().getFullYear() - new Date(dob).getFullYear();
+
+        // Check if email or username already exists
+        const existingUser = await prisma.user.findFirst({
+            where: { email },
+        });
+
+        if (existingUser) {
+            return NextResponse.json(
+                { message: "Email or Username already taken" },
+                { status: 400 }
+            );
+        }
+
+        // Ensure height and weight are stored as numbers
+        const parsedHeight = parseFloat(height);
+        const parsedWeight = parseFloat(weight);
+ 
+
+        // Ensure gender is stored in uppercase
+        const formattedGender = gender.toUpperCase();
+
+        // Create a new user with the provided details
+        const newUser = await prisma.user.create({
+            data: {
+                fullName,
+                userName: username,
+                email,
+                password,
+                googleId : Date.now() + username,
+                dob: new Date(dob),
+                age,
+                gender: formattedGender,
+                height: parsedHeight,
+                weight: {
+                    create: {
+                        date: new Date(),
+                        weight: parsedWeight,
+                    },
+                }, // Add weight record
+                additionalInfo,
+                preferences: {
+                    create: {
+                        activityLevel,
+                        preferredExerciseType,
+                        exerciseExperience: experience,
+                        workoutDuration,
+                        exerciseFrequency,
+                        macronutrientPreferences: macroNutrientPreferences,
+                        fitnessGoals: goal,
+                        pace,
+                        availableEquipments,
+                    },
+                },
+                healthAndDietary: {
+                    create: {
+                        dietaryPreferences,
+                        healthProblems,
+                        allergies,
+                    },
+                },
+            },
+        });
+
         return NextResponse.json(
-            { message: "Internal Server Error" },
+            { message: "User created successfully", userId: newUser.id },
+            { status: 200 }
+        );
+    } catch (error) {
+        console.log(`Error creating user: ${error}`);
+        return NextResponse.json(
+            { message: "Internal Server Error" , error},
             { status: 500 }
         );
     }
